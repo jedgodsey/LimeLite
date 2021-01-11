fd07f626617e251bd8e19e3203381526e064e1de
jedgodsey <jed.godsey@gmail.com>
2021-01-11 15:31:08 -0800

    added run file

--- limelite_project/settings.py
+++ limelite_project/settings.py
@@ -14,12 +14,6 @@ import dotenv
 
 from pathlib import Path
 
-# .env variables
-dotenv_file = os.path.join(BASE_DIR, ".env")
-if os.path.isfile(dotenv_file):
-    dotenv.load_dotenv(dotenv_file)
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -27,8 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
+# .env variables
+dotenv_file = os.path.join(BASE_DIR, ".venv")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['thisissomekindofsecret']
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
